8d34d12b06f1570f5b371bb96de4af09c1517a12
mmaka4 <saidimmaka@gmail.com>
2020-09-08 19:55:08 +0300

    Initial commit

---
+++ app/src/main/java/com/example/gadsleaderboard/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.gadsleaderboard
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(),  View.OnClickListener {
+
+    lateinit var button : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val fragmentAdapter = TabAdapter(supportFragmentManager)
+        viewPager.adapter = fragmentAdapter
+
+        tabLayout.setupWithViewPager(viewPager)
+
+        button = findViewById(R.id.submit_btn)
+        button.setOnClickListener(this)
+
+    }
+
+    override fun onClick(view: View?) {
+        when(view?.id) {
+            R.id.submit_btn->{
+                startActivity(Intent(this, ProjectSubmit::class.java))
+                Toast.makeText(applicationContext, "لقد أدخلت شهر غير صالح", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}

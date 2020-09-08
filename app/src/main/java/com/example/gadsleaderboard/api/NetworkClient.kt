9beaa07d53900814233035cf932b84dfc07e7074
mmaka4 <saidimmaka@gmail.com>
2020-09-08 21:30:37 +0300

    NetworkClient file added

---
+++ app/src/main/java/com/example/gadsleaderboard/api/NetworkClient.kt
@@ -0,0 +1,25 @@
+package com.example.gadsleaderboard.api
+
+import com.google.gson.GsonBuilder
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+object NetworkClient {
+    const val BASE_URL = "https://gadsapi.herokuapp.com"
+    private var retrofit: Retrofit? = null
+    fun getRetrofitClient(): Retrofit? {
+        if (retrofit == null) {
+
+            val gson =  GsonBuilder()
+                .setLenient()
+                .create()
+
+            retrofit = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build()
+        }
+        return retrofit
+    }
+}

8b6ade10363f75c3616d769afc63372f008b8dd5
mmaka4 <saidimmaka@gmail.com>
2020-09-09 21:25:29 +0300

    confirm pop up

--- app/src/main/java/com/example/gadsleaderboard/ProjectSubmit.kt
+++ app/src/main/java/com/example/gadsleaderboard/ProjectSubmit.kt
@@ -1,9 +1,16 @@
 package com.example.gadsleaderboard
 
+import android.annotation.SuppressLint
 import android.app.Activity
+import android.app.Dialog
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.Window
+import android.widget.Button
 import android.widget.ImageView
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
 class ProjectSubmit : AppCompatActivity(), View.OnClickListener {
@@ -16,6 +23,9 @@ class ProjectSubmit : AppCompatActivity(), View.OnClickListener {
 
         imageView = findViewById(R.id.back_arrow)
         imageView.setOnClickListener(this)
+
+        //dialog()
+        showDialog()
     }
 
     override fun onClick(view: View?) {
@@ -24,5 +34,48 @@ class ProjectSubmit : AppCompatActivity(), View.OnClickListener {
                 finish()
             }
         }
+    }
+
+     fun showDialog() {
+        val dialog = Dialog(this, R.style.CustomDialogTheme)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(false)
+        dialog.setContentView(R.layout.confirm_pop_up)
+//         dialog.window?.setDimAmount(0.65f)
+        val cancelBtn = dialog.findViewById(R.id.cancel_btn) as ImageView
+        val yesBtn = dialog.findViewById(R.id.yes_btn) as Button
+        yesBtn.setOnClickListener {
+            dialog.dismiss()
+        }
+        cancelBtn.setOnClickListener { dialog.dismiss() }
+        dialog.show()
+         Log.i("ProjectSumit", "showDialog")
+    }
+
+    @SuppressLint("InflateParams")
+    fun dialog() {
+        val mDialogView = LayoutInflater.from(this).inflate(R.layout.confirm_pop_up, null)
+        //AlertDialogBuilder
+        val mBuilder = AlertDialog.Builder(this)
+            .setView(mDialogView)
+        mBuilder.show()
+        //show dialog
+//        val  mAlertDialog = mBuilder.show()
+        //login button click of custom layout
+//        mDialogView.dialogLoginBtn.setOnClickListener {
+//            //dismiss dialog
+//            mAlertDialog.dismiss()
+//            //get text from EditTexts of custom layout
+//            val name = mDialogView.dialogNameEt.text.toString()
+//            val email = mDialogView.dialogEmailEt.text.toString()
+//            val password = mDialogView.dialogPasswEt.text.toString()
+//            //set the input text in TextView
+//            mainInfoTv.setText("Name:"+ name +"\nEmail: "+ email +"\nPassword: "+ password)
+//        }
+//        //cancel button click of custom layout
+//        mDialogView.dialogCancelBtn.setOnClickListener {
+//            //dismiss dialog
+//            mAlertDialog.dismiss()
+//        }
     }
 }

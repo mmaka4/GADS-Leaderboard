00172f2ed5978d44740cec8fc5a3f2b32d90ae90
mmaka4 <saidimmaka@gmail.com>
2020-09-09 14:42:45 +0300

    SkillFragment modifications

--- app/src/main/java/com/example/gadsleaderboard/SkillFragment.kt
+++ app/src/main/java/com/example/gadsleaderboard/SkillFragment.kt
@@ -1,18 +1,31 @@
 package com.example.gadsleaderboard
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.example.gadsleaderboard.adapter.LearnerAdapter
+import com.example.gadsleaderboard.model.Learner
 
 class SkillFragment : Fragment() {
+
+    private lateinit var rootView: View
+    lateinit var recyclerView: RecyclerView
+    lateinit var learnerAdapter: LearnerAdapter
+    lateinit var fragmentContext: Context
+    lateinit var learnersList: ArrayList<Learner>
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_skilliq, container, false)
+        rootView = inflater.inflate(R.layout.fragment_skilliq, container, false)
+        fragmentContext = this.context!!
+
+        return rootView
     }
 }

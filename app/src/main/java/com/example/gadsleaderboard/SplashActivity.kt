f520a05f461a8f581c416c36d429841dc553692d
mmaka4 <saidimmaka@gmail.com>
2020-09-10 18:14:15 +0300

    splash things

--- app/src/main/java/com/example/gadsleaderboard/SplashActivity.kt
+++ app/src/main/java/com/example/gadsleaderboard/SplashActivity.kt
@@ -1,4 +1,17 @@
 package com.example.gadsleaderboard
 
-class SplashActivity {
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.splash_layout)
+
+//        startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+        //finish()
+    }
 }

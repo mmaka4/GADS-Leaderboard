e9319bf18527847094c0aedb3937f50aab449ab9
mmaka4 <saidimmaka@gmail.com>
2020-09-10 23:26:23 +0300

    learn layout shimmer effect

--- app/src/main/java/com/example/gadsleaderboard/ProjectSubmit.kt
+++ app/src/main/java/com/example/gadsleaderboard/ProjectSubmit.kt
@@ -3,6 +3,7 @@ package com.example.gadsleaderboard
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.Dialog
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -12,10 +13,25 @@ import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isNotEmpty
+import com.example.gadsleaderboard.api.ServerApi
+import com.google.gson.Gson
+import kotlinx.android.synthetic.main.project_submit_layout.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class ProjectSubmit : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var imageView : ImageView
+    private lateinit var submitProjectBtn : Button
+    private val submitBaseUrl = "https://docs.google.com/forms/d/e/"
+    var emailTxt: String = ""
+    var firstName: String = ""
+    var lastName: String = ""
+    var projectLink: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +39,14 @@ class ProjectSubmit : AppCompatActivity(), View.OnClickListener {
 
         imageView = findViewById(R.id.back_arrow)
         imageView.setOnClickListener(this)
+
+        submitProjectBtn = findViewById(R.id.submit_project_btn)
+        submitProjectBtn.setOnClickListener(this)
+
+        emailTxt = fName?.text.toString()
+        firstName = lName?.text.toString()
+        lastName = eMail?.text.toString()
+        projectLink = pLink?.text.toString()
 
 //        showSuccessDialog()
 //        showFailureDialog()
@@ -32,6 +56,11 @@ class ProjectSubmit : AppCompatActivity(), View.OnClickListener {
         when(view?.id) {
             R.id.back_arrow -> {
                 finish()
+            }
+            R.id.submit_project_btn -> {
+                if (email.isNotEmpty() && firstName.isNotEmpty() && lastName.isNotEmpty() && projectLink.isNotEmpty()) {
+                    Log.i("EditText Values", "$email $firstName $lastName $projectLink")
+                }
             }
         }
     }
@@ -74,4 +103,43 @@ class ProjectSubmit : AppCompatActivity(), View.OnClickListener {
         Log.i("ProjectSumit", "showDialog")
     }
 
+//    private fun update(id:String, fruitName:String, fruitPrice:String){
+//
+//        Log.i("EditText Values",id+" "+fruitName+" "+fruitPrice)
+//        val retrofit = Retrofit.Builder().baseUrl(submitBaseUrl).addConverterFactory(
+//            GsonConverterFactory.create()).build()
+//
+//        val api = retrofit.create(ServerApi::class.java)
+//
+//        val call = api.submitProject(id, fruitName, fruitPrice)
+//
+//        val gson = Gson()
+//
+//        call.enqueue(object : Callback<MatundaResponse> {
+//
+//            override fun onResponse(
+//                call: Call<MatundaResponse>,
+//                response: Response<MatundaResponse>
+//            ) {
+//                Log.i("ResponseString",gson.toJson(response.body()))
+//
+//                if(response.isSuccessful){
+//                    if (response.body()?.status!!){
+//                        val intent = Intent(this@UpdateActivity, ListFruits::class.java)
+//                        startActivity(intent)
+//                    }
+//
+////                    response.body()?.matunda
+//                }else{
+//
+//                }
+//            }
+//
+//            override fun onFailure(call: Call<MatundaResponse>, t: Throwable) {
+//                Log.i("ResponseFailure1",t.message)
+//            }
+//
+//        })
+//
+//    }
 }
--- app/src/main/java/com/example/gadsleaderboard/api/ServerApi.kt
+++ app/src/main/java/com/example/gadsleaderboard/api/ServerApi.kt
@@ -12,4 +12,13 @@ interface ServerApi {
 
     @GET("/api/skilliq")
     fun getSkills(): Call<ArrayList<Skill>>
+
+    @FormUrlEncoded
+    @POST("1FAIpQLSf9d1TcNU6zc6KR8bSEM41Z1g1zl35cwZr2xyjIhaMAz8WChQ/formResponse")
+    fun submitProject(
+        @Field("entry.1824927963") email: String,
+        @Field("entry.1877115667") firstName: String,
+        @Field("entry.2006916086") lastName: String,
+        @Field("entry.284483984") projectLink: String
+    ): Call<Void>
 }

2e370fdb3ef1834049dbe770b6969a24149c6537
mmaka4 <saidimmaka@gmail.com>
2020-09-10 21:39:00 +0300

    learn layout shimmer effect

--- app/src/main/java/com/example/gadsleaderboard/LearningFragment.kt
+++ app/src/main/java/com/example/gadsleaderboard/LearningFragment.kt
@@ -1,14 +1,17 @@
 package com.example.gadsleaderboard
 
 import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.example.gadsleaderboard.adapter.LearnerAdapter
 import com.example.gadsleaderboard.api.NetworkClient
 import com.example.gadsleaderboard.api.ServerApi
@@ -25,9 +28,11 @@ class LearningFragment : Fragment() {
 
     private lateinit var rootView: View
     lateinit var recyclerView: RecyclerView
+    lateinit var learnRefresher: SwipeRefreshLayout
     lateinit var learnerAdapter: LearnerAdapter
     lateinit var fragmentContext: Context
     lateinit var learnersList: ArrayList<Learner>
+    private var listInitialized = false
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -37,7 +42,29 @@ class LearningFragment : Fragment() {
         rootView = inflater.inflate(R.layout.fragment_learning, container, false)
         fragmentContext = this.context!!
         initializeRecyclerView()
+        swipeToRefresh()
+
         return rootView
+    }
+
+    private fun swipeToRefresh() {
+        //** Set the colors of the Pull To Refresh View
+        learnRefresher = rootView.findViewById(R.id.learnSwipeRefresher)
+        learnRefresher.setProgressBackgroundColorSchemeColor(
+            ContextCompat.getColor(
+                fragmentContext,
+                R.color.colorPrimary
+            )
+        )
+        learnRefresher.setColorSchemeColors(Color.WHITE)
+
+        learnRefresher.setOnRefreshListener {
+            if (listInitialized)
+                learnersList.clear()
+
+            loadLearners()
+            learnRefresher.isRefreshing = false
+        }
     }
 
     private fun initializeRecyclerView() {

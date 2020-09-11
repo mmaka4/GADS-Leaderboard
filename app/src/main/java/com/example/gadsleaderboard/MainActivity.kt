455027e75c7c96e4c33650d5fbec5dd0c26c1d98
mmaka4 <saidimmaka@gmail.com>
2020-09-11 19:55:46 +0300

    pop up updates

--- app/src/main/java/com/example/gadsleaderboard/MainActivity.kt
+++ app/src/main/java/com/example/gadsleaderboard/MainActivity.kt
@@ -35,7 +35,7 @@ class MainActivity : AppCompatActivity(),  View.OnClickListener {
         when(view?.id) {
             R.id.submit_btn->{
                 startActivity(Intent(this, ProjectSubmit::class.java))
-                Toast.makeText(applicationContext, "Submit Activity", Toast.LENGTH_SHORT).show()
+                //Toast.makeText(applicationContext, "Submit Activity", Toast.LENGTH_SHORT).show()
             }
         }
     }
